db8e4844027a975c66da75bc19e721927d5e988a
KyeongHoon Kim <71899948+khoon-git@users.noreply.github.com>
2022-03-23 19:23:00 +0900

    Update SMTPAuthenticatior.java

--- 자전거시그널/src/kkh/SMTPAuthenticatior.java
+++ 자전거시그널/src/kkh/SMTPAuthenticatior.java
@@ -7,7 +7,7 @@ public class SMTPAuthenticatior extends Authenticator{
  
     @Override
     protected PasswordAuthentication getPasswordAuthentication() {
-        return new PasswordAuthentication("rudgnssla121","dnflrkwhr12");
+        return new PasswordAuthentication("","");
     }
 }
 
